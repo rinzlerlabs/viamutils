2de19b7665c82ec6a0f8740805b24814729ce374
TheGreatCO <thegreatco@thegreatco.com>
2024-11-11 09:37:55 -0500

    add some more logging to module run logic

--- module/startup.go
+++ module/startup.go
@@ -24,24 +24,30 @@ func AddModularResource(api resource.API, model resource.Model) error {
 }
 
 func RunModule(ctx context.Context, args []string, logger logging.Logger) error {
+	logger.Infof("Starting Viam Module with args: %v", args)
 	module, err := module.NewModuleFromArgs(ctx, logger)
 	if err != nil {
 		logger.Errorf("Failed to start module: %v", err)
 		return err
 	}
+	logger.Infof("Module started successfully, registering %v models", len(components))
 	for _, component := range components {
+		logger.Infof("Adding %v to module with API %v", component.Model, component.API)
 		err = module.AddModelFromRegistry(ctx, component.API, component.Model)
 		if err != nil {
 			logger.Errorf("Failed to add model to module: %v", err)
 			return err
 		}
 	}
+	logger.Info("Starting module")
 	err = module.Start(ctx)
 	if err != nil {
 		logger.Errorf("Failed to start module: %v", err)
 		return err
 	}
 	defer module.Close(ctx)
+	logger.Info("Module started successfully")
 	<-ctx.Done()
+	logger.Info("Module stopped")
 	return nil
 }
